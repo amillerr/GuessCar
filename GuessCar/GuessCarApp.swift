9e82d3fb5f029bc65ae74754c5b57d3245f51642
Tom Miller <62253468+amillerr@users.noreply.github.com>
2022-10-19 12:06:56 +0300

    Initial Commit

---
+++ GuessCar/GuessCarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessCarApp.swift
+//  GuessCar
+//
+//  Created by Artem Axenov on 2022-10-19.
+//
+
+import SwiftUI
+
+@main
+struct GuessCarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
